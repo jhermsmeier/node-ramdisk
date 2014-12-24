fbf50e1e7cd05eb70ccaac301015461ed937e551
jhermsmeier <jhermsmeier@googlemail.com>
2014-12-24 20:54:46 +0100

    Updated test: added constructor tests

--- test/index.js
+++ test/index.js
@@ -1,13 +1,40 @@
 var assert = require( 'assert' )
 var RamDisk = require( '../' )
 
-describe( 'RAMDISK', function() {
+describe( 'RamDisk', function() {
   
   describe( 'contructor', function() {
     
     it( 'should work with default options', function() {
       var device = new RamDisk()
       assert.ok( device instanceof RamDisk )
+    })
+    
+    it( 'should throw if blockSize != power of 2', function() {
+      assert.throws( function() {
+        var device = new RamDisk({ blockSize: 123 })
+      })
+    })
+    
+    it( 'should throw if clusterSize != multiple of blockSize', function() {
+      assert.throws( function() {
+        var device = new RamDisk({ clusterSize: 256 })
+      })
+      assert.throws( function() {
+        var device = new RamDisk({ clusterSize: 1230 })
+      })
+    })
+    
+    it( 'should throw if size < clusterSize', function() {
+      assert.throws( function() {
+        var device = new RamDisk({ size: 2048 })
+      })
+    })
+    
+    it( 'should throw if size exceeds INT_MAX', function() {
+      assert.throws( function() {
+        var device = new RamDisk({ size: Math.pow( 2, 64 ) })
+      })
     })
     
   })
